1716f8ac1059730ff761caf2a22626e51a3874a5
Alexander Ibraimov <59023460+AlexanderIbraimov@users.noreply.github.com>
2021-12-10 01:02:33 +0600

    Created a simple express application

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const { calculateEquity } = require('poker-odds');
 
+
+try {
+    const app = express();
+    app.use("/oddsHandler", function(request, response){
+    });
+    app.listen(8080);
+} catch (error) {
+    
+}
+
