8cf4ce3fd996a4fe12aac426dc7fc6094968f970
Alexander Ibraimov <59023460+AlexanderIbraimov@users.noreply.github.com>
2021-12-10 01:04:44 +0600

    Added parsing of cards

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ try {
     });
     app.listen(8080);
 } catch (error) {
+    console.log(error);
+}
+
+
+function parserPlayerCards(playerCards) {
+    let hands = playerCards.split(';').filter(c=>c);;
+    let result = [];
     
+    hands.forEach(hand => {
+        var cards = hand.split(',').filter(c=>c).map(c=> fixCard(c));
+        result.push(cards);
+    });
+    
+    return result;
+}
+
+function parserTableCards(tableCards){
+    return tableCards.split(',').filter(c=>c).map(c=>fixCard(c));
+}
+
+function fixCard(card){
+    var cardType = card.substring(0, 1);
+    var cardValue = card.substring(1);
+    if (cardValue=='10')
+    {
+        cardValue = 'T';
+    }
+    
+    return `${cardValue.toUpperCase()}${cardType.toLowerCase()}`;
 }
 
