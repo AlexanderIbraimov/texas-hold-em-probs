4ada09c58066a3ca406b971272b12706bcbbdfc2
Alexander Ibraimov <59023460+AlexanderIbraimov@users.noreply.github.com>
2021-12-16 21:15:31 +0600

    Created a method to get the result

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const { calculateEquity } = require('poker-odds');
-
 
 const iterations = 1000000000; 
 const exhaustive = false; 
@@ -14,8 +12,9 @@ try {
         parserPlayerCards(playerCards);
         let board = parserTableCards(tableCards);
         let hands = parserPlayerCards(playerCards);
-        
-        response.send(JSON.stringify(calculateEquity(hands, board, iterations, exhaustive)));
+        var quity = calculateEquity(hands, board, iterations, exhaustive);
+        console.log(quity);
+        response.send(JSON.stringify(getProbabilityResult(quity)));
     });
     app.listen(8080);
 } catch (error) {
@@ -48,4 +47,47 @@ function fixCard(card){
     }
     
     return `${cardValue.toUpperCase()}${cardType.toLowerCase()}`;
+}
+
+function getProbabilityResult(quity){
+        var playerStats=[];
+
+        for(var hand=0; hand < 6;hand++){
+            var handData = {
+                'winProb':0.0,
+                'tieProb':{
+                    'probability':0.0,
+                    'count':0
+                }
+            };
+            playerStats.push(handData);
+        }
+
+        return {
+            'playerStats':playerStats,
+            'handStats': {
+                'royalFlush':0.0,
+                'straightFlush':0.0,
+                'fourOfAKind':0.0,
+                'fullHouse':0.0,
+                'flush':0.0,
+                'straight':0.0,
+                'threeOfAKind':0.0,
+                'twoPairs':0.0,
+                'onePair':0.0,
+                'highCard':0.0
+            },
+            'additionalStats':{
+                'redsOver25':0.0,
+                'redsUnder25':0.0,
+                'clubsOver15':0.0,
+                'clubsUnder15':0.0,
+                'heartsOver15':0.0,
+                'heartsUnder15':0.0,
+                'diamondsOver15':0.0,
+                'diamondsUnder15':0.0,
+                'spadesOver15':0.0,
+                'spadesUnder15':0.0,
+            }
+        }
 }
